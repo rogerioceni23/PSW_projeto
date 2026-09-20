1993dc045fab886948651879c40102f3a07d9c3d
rogerioceni23 <146475238+rogerioceni23@users.noreply.github.com>
2026-09-20 16:26:48 -0300

    adiciona formularios de autor e usuarios

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -1,12 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
-from .models import Categoria, Livro
+from .models import Autor, Categoria, Livro, Usuario, UsuarioLivro
 
 
 class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ["nome"]
+
 
 class LivroForm(forms.ModelForm):
     class Meta:
@@ -19,4 +21,60 @@ class LivroForm(forms.ModelForm):
             "arquivo_pdf",
             "categorias",
             "autores",
+        ]
+
+
+class AutorForm(forms.ModelForm):
+    class Meta:
+        model = Autor
+        fields = [
+            "nome",
+            "biografia",
+            "data_nascimento",
+        ]
+
+
+class UsuarioLivroForm(forms.ModelForm):
+    class Meta:
+        model = UsuarioLivro
+        fields = [
+            "usuario",
+            "livro",
+            "data_hora",
+        ]
+        widgets = {
+            "data_hora": forms.DateTimeInput(
+                attrs={"type": "datetime-local"}
+            ),
+        }
+
+
+class UsuarioCriacaoForm(UserCreationForm):
+    class Meta:
+        model = Usuario
+        fields = [
+            "username",
+            "nome",
+            "rg",
+            "cpf",
+            "endereco",
+            "foto",
+            "descricao",
+            "groups",
+        ]
+
+
+class UsuarioEdicaoForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = [
+            "username",
+            "nome",
+            "rg",
+            "cpf",
+            "endereco",
+            "foto",
+            "descricao",
+            "groups",
+            "is_active",
         ]
